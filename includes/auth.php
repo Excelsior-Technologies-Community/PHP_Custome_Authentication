74d360294daf351519f97069e5da5dbd23c9a496
patelmanasi <manasipatel0852@gmail.com>
2026-04-16 14:48:51 +0530

    Enhance custom authentication with Remember Me and password toggle (show/hide password)

--- includes/auth.php
+++ includes/auth.php
@@ -1,12 +1,7 @@
 <?php
-// includes/auth.php
-
-// Start PHP session to access session variables
 session_start();
 
-// Check if the user is logged in by looking for 'custome_id' in session
 if (!isset($_SESSION['custome_id'])) {
-    // If not logged in, redirect the user to the login page
-    header("Location: login.php");
-    exit; // Stop further script execution
+    header("Location: /PHP_Custome_Authentication/public/login.php");
+    exit;
 }
